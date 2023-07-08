f716503a5cacd8e1191470f77cd1c1e01c16bd85
SadMin <vegita796@gmail.com>
2023-07-07 21:47:21 -0500

    Juego sencillito

---
+++ madLibs.py
@@ -0,0 +1,8 @@
+adj = input("Adjetivo: ")
+verb1 = input("Verbo: ")
+verb2 = input("Verbo: ")
+sustantivo = input ("Sustantivo: ")
+
+madlib = f"Jugar es tan {adj}! Los mundos son tan abiertos que {verb1} en su mundo, siempre se aprende algo nuevo que me llena de {verb2}, por eso juego cuando estoy {sustantivo}"
+
+print(madlib)
